2c298e9db65f8e12f97527c9342114d433f732e0
Jelani Alexander <jelanialexander82@gmail.com>
2023-02-15 17:48:15 +0900

    added clientQueryProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const queryClient = new QueryClient({});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-    
-  </React.StrictMode>
-)
+  </QueryClientProvider>
+);
