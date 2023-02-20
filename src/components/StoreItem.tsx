94f94b4a21656cbd8dadf486398c830fbaec84fd
Jelani Alexander <jelanialexander82@gmail.com>
2023-02-20 16:54:55 +0900

    formated card-price component

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -2,7 +2,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions, Grid } from "@mui/material";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
 type StoreItemProps = {
   id: number;
@@ -20,33 +20,34 @@ export function StoreItem({
   description,
 }: StoreItemProps) {
   return (
-    <>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia component="img" height="140" image={imgUrl} alt={name} />
-          <CardContent
-            sx={{ display: "flex", justifyContent: "space-between" }}>
-            <div>
-              <Typography gutterBottom variant="h5" component="div">
-                {name}
-              </Typography>
-            </div>
-            <div>
-              <Typography variant="h5" color="text.secondary">
-                {price}
-              </Typography>
-            </div>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-        </CardActions>
-      </Card>
-    </>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia component="img" height="140" image={imgUrl} alt={name} />
+        <CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            <Typography gutterBottom variant="h5" component="div">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+          </div>
+          <div>
+            <Typography variant="h5" color="textPrimary">
+              {price}
+            </Typography>
+          </div>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
+
 
 {
   /* <Typography variant="body2" color="text.secondary">
