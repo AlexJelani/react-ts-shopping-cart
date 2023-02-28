cb251dd8172d1f8ccef5dcabd97ec479207d63d2
Jelani Alexander <jelanialexander82@gmail.com>
2023-02-28 11:48:53 +0900

    added close button

--- src/components/ShoppingCartComponent.tsx
+++ src/components/ShoppingCartComponent.tsx
@@ -1,6 +1,10 @@
+import { IconButton, Typography } from "@mui/material";
 import Drawer from "@mui/material/Drawer";
 import { Wrapper } from "../App.styles";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import CancelIcon from "@mui/icons-material/Cancel";
+import { flexbox } from "@mui/system";
+import { blue } from "@mui/material/colors";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -13,13 +17,27 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
     <Wrapper>
       <Drawer
         PaperProps={{
-            sx: { width: "50%" },
-          }}
+          sx: { width: "50%" },
+        }}
         anchor="right"
         open={isOpen}
         onClose={closeCart}
       >
-        <h1>Your Cart</h1>
+        {" "}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginRight: 10,
+          }}
+        >
+          <Typography variant="h4" m={4}>
+            Your cart
+          </Typography>
+          <IconButton onClick={closeCart} sx={{color:'#0d47a1'}}>
+            <CancelIcon  fontSize="large"/>
+          </IconButton>
+        </div>
       </Drawer>
     </Wrapper>
   );
