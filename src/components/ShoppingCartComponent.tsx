fc716127930280257fa13e3bc720246e63d10b32
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-01 06:41:30 +0900

    added CartItems component

--- src/components/ShoppingCartComponent.tsx
+++ src/components/ShoppingCartComponent.tsx
@@ -3,6 +3,7 @@ import Drawer from "@mui/material/Drawer";
 import { Wrapper } from "../App.styles";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import CancelIcon from "@mui/icons-material/Cancel";
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import { flexbox } from "@mui/system";
 import { blue } from "@mui/material/colors";
 
@@ -11,7 +12,7 @@ type ShoppingCartProps = {
 };
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
-  const { closeCart } = useShoppingCart();
+  const { closeCart, cartItems } = useShoppingCart();
 
   return (
     <Wrapper>
@@ -32,14 +33,16 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
           }}
         >
           <Typography variant="h4" m={4}>
-            Your cart
+            Your shopping cart <ShoppingBagIcon fontSize="large" sx={{color:'#0d47a1'}}/>
           </Typography>
           <IconButton onClick={closeCart} sx={{color:'#0d47a1'}}>
             <CancelIcon  fontSize="large"/>
           </IconButton>
         </div>
         <List>
-          
+          {cartItems.map(item =>(
+          <CartItem key={item.id} {...item}/>
+          ))}
         </List>
       </Drawer>
     </Wrapper>
