ffcaf4f9f234318eeae1c8ffdd789f8fff62a002
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-01 21:50:20 +0900

    deleted Semantic Ui button

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,7 +1,6 @@
 import { List, Stack } from "@mui/material";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import storeItems from "../data/items.json";
-import { Button } from "semantic-ui-react";
 
 type CartItemProps = {
   id: number;
@@ -14,11 +13,16 @@ export function CartItem({ id, quantity }: CartItemProps) {
   if (item === null) return null;
 
   return (
-    
-    <List component={Stack} direction="row" gap={0} >
-      <img src={item?.imgUrl} style={{ width: "125px", height: "75px", padding:"25px", objectFit:"cover" }}  />
-      <button className="ui button" role="button">Btn with Semantic-UI</button>
-
+    <List component={Stack} direction="row" gap={0}>
+      <img
+        src={item?.imgUrl}
+        style={{
+          width: "125px",
+          height: "75px",
+          padding: "25px",
+          objectFit: "cover",
+        }}
+      />
     </List>
   );
 }
