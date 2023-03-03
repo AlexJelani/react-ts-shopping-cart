00db95037e075b3c7eea792c2bd517530cb47122
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-04 07:37:31 +0900

    add Json data

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -6,7 +6,7 @@ import { StoreItem } from "../components/StoreItem";
 export function Store() {
   return (
     <>
-      <h1>Pet Store</h1>
+      <h1>T-Shirt Store</h1>
       <Grid container spacing={2}>
         {storeItems.map((item) => (
           <Grid item xs={12} sm={6} md={4} key={item.id}>
