17f489937f8fa88c744a87015f07245b55ba4bfc
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-04 10:46:37 +0900

    added delete button

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,12 +1,16 @@
 import {
   Box,
+  Button,
   Card,
   CardContent,
   CardMedia,
+  IconButton,
   Typography,
 } from "@mui/material";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import storeItems from "../data/items.json";
+import { formatCurrency } from "../utilities/formatCurrency";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 type CartItemProps = {
   id: number;
@@ -20,19 +24,44 @@ export function CartItem({ id, quantity }: CartItemProps) {
 
   return (
     <Card variant="outlined" sx={{ width: "55%" }}>
-    <Box sx={{ display: "flex", alignItems: "center" }}>
-      <CardMedia
-        component="img"
-        image={item?.imgUrl}
-        style={{ width: "125px", height: "100%", objectFit: "cover" }}
-      />
-      <CardContent>
-        <Typography variant="body2" component="div">
-          {item?.name}
-        </Typography>
-      </CardContent>
-      <Box sx={{ flexGrow: 1 }} />
-    </Box>
-  </Card>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <CardMedia
+            component="img"
+            image={item?.imgUrl}
+            style={{ width: "125px", height: "100%", objectFit: "cover" }}
+          />
+          <CardContent>
+            <Typography variant="body2" component="div">
+              {item?.name}
+              {quantity > 1 && (
+                <span
+                  className="text-muted"
+                  style={{ fontSize: ".75rem", color: "green" }}
+                >
+                  {" "}
+                  x{quantity}
+                </span>
+              )}
+              <div>{formatCurrency(item?.price)}</div>
+            </Typography>
+          </CardContent>
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Typography variant="h6" component="div" sx={{ mr: 1 }}>
+            {formatCurrency(item?.price * quantity)}
+          </Typography>
+          <IconButton size="small" color="error" onClick={() => removeFromCart(id)}>
+            <DeleteForeverIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </Card>
   );
 }
--- src/utilities/formatCurrency.ts
+++ src/utilities/formatCurrency.ts
@@ -1,8 +1,12 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined,{
-    currency: "USD", style: "currency"})
-
-
-export function formatCurrency(number:number) {
-    return CURRENCY_FORMATTER.format(number)
-
-}
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+    currency: "USD",
+    style: "currency"
+  })
+  
+  export function formatCurrency(number: number | undefined) {
+    if (typeof number === 'number') {
+      return CURRENCY_FORMATTER.format(number)
+    }
+    return ''
+  }
+  
