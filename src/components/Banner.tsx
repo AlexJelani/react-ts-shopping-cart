7a599cd183765256918dfe46533d7a3e83aef184
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-08 20:31:01 +0900

    added Bannercontent

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,32 @@
-import { useTheme } from "@mui/material";
+import { Button, Typography, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/system";
+import {
+  BannerContainer,
+  BannerContent,
+  BannerDescription,
+  BannerImage,
+  BannerShopButton,
+  BannerTitle,
+} from "../styles/banner/index";
 
 export default function Banner() {
   const theme = useTheme();
 
   return (
     <>
-      <h1>Banner</h1>
+      <BannerContainer>
+        <BannerContent>
+          <Typography variant="h6">
+            Huge content
+          </Typography>
+          <BannerTitle variant="h2">New Bags</BannerTitle>
+          <BannerDescription variant="subtitle1">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Iste repudiandae accusantium minima natus repellat deleniti
+             accusamus quibusdam doloribus deserunt laudantium.
+          </BannerDescription>
+        </BannerContent>
+      </BannerContainer>
     </>
   );
 }
