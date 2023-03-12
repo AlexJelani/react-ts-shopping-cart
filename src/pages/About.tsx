92fcf3c01e74ba27b840e731ad77a209beac65df
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-12 19:43:04 +0900

    added href links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
-import '../style/home.scss'
-import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
+import "../style/home.scss";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 export function About() {
   return (
     <>
-       <div id="home-two">
-        <Link to={"/store"}>
-          <p id="shopBtn"><FaGithub/><FaLinkedinIn/></p>
-        </Link>
+      <div id="home-two">
+        <p id="shopBtn">
+          <a
+            href="https://github.com/AlexJelani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub style={{ marginRight: "10px" }} />
+          </a>
+          <a
+            href="https://linkedin.com/in/jelani-alexander"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn />
+          </a>
+        </p>
       </div>
     </>
   );
