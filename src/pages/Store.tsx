7352f7e52e88bb9257f020edf0277826b52d4ce4
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-14 12:01:33 +0900

    add container to store

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,19 +1,20 @@
-
-import {  Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import storeItems from "../data/items.json";
 import { StoreItem } from "../components/StoreItem";
 
 export function Store() {
   return (
     <>
-      <Grid container spacing={1} paddingTop="1rem">
-        {storeItems.map((item) => (
-          <Grid item xs={12} sm={6} md={4} key={item.id}>
-            <StoreItem{...item}/>
-          </Grid>
-        ))}
-      </Grid>
+      {" "}
+      <Container>
+        <Grid container spacing={1} paddingTop="1rem">
+          {storeItems.map((item) => (
+            <Grid item xs={12} sm={6} md={4} key={item.id}>
+              <StoreItem {...item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
   );
 }
-
