d01cfd20790ddb5054b5dc880a6ae92920833942
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-25 18:26:47 +0900

    removed overflow anded css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,39 @@
+import { Box, Button, Container } from "@mui/material";
 import { Link } from "react-router-dom";
-import '../style/home.scss'
+import backgroundImage from "/imgs/anomaly-WWesmHEgXDs-unsplash.jpg";
+
+const styles = {
+  home: {
+    height: "100vh",
+    width: "100%",
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundBlendMode: "luminosity",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  shopBtn: {
+    fontFamily: "Comfortaa, cursive",
+    color: "#eaeaea",
+    backgroundColor: "black",
+    fontSize: "5em",
+    padding: "1em",
+    cursor: "pointer",
+    boxShadow: "10px 10px 10px gray",
+  },
+};
 
 export function Home() {
   return (
-    <>
-      <div id="home">
+    <Box sx={styles.home}>
+      <Container>
         <Link to={"/store"}>
-          <p id="shopBtn">Go Shopping</p>
+          <Button sx={styles.shopBtn}>Go Shopping</Button>
         </Link>
-      </div>
-    </>
+      </Container>
+    </Box>
   );
 }
